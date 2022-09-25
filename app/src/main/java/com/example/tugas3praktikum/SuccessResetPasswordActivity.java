63a9768eba1024476c06adac48cf6a75474b1084
Muhammad Althaaf Abqary <taftaraharjo@gmail.com>
2022-09-25 14:00:57 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tugas3praktikum/SuccessResetPasswordActivity.java
@@ -0,0 +1,14 @@
+package com.example.tugas3praktikum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SuccessResetPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_success_reset_password);
+    }
+}
